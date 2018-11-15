7105263e818f07c7f404b402c7f65ae79ffa16f8
Devon Yanitski <devon7y@gmail.com>
2018-11-14 21:49:16 -0700

    Finished the second part of the app.

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -12,6 +12,8 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var blurBar: UIVisualEffectView!
+    @IBOutlet weak var hotdogLabel: UILabel!
     
     let imagePicker = UIImagePickerController()
     
@@ -21,6 +23,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         imagePicker.delegate = self
         imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
+        
+        blurBar.layer.cornerRadius = 35
+        blurBar.clipsToBounds = true
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -30,10 +35,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             imageView.image = userPickedImage
             
+            guard let ciimage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert.")
+            }
+            
+            detect(image: ciimage)
+        
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
+    }
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Failed to prosess image.")
+            }
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.hotdogLabel.text = "Hotdog"
+                } else {
+                    self.hotdogLabel.text = "Not a hotdog"
+                }
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
